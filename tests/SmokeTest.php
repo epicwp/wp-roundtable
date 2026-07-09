5097825276ff970ff19b619fa86dae3f797c0389
Robbert Vermeulen <robbertvermeulen@gmail.com>
2026-07-09 14:45:04 +0200

    chore(test): add PHPUnit + Brain Monkey harness and CI test step

---
+++ tests/SmokeTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace EpicWP\Roundtable\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+final class SmokeTest extends PHPUnitTestCase
+{
+    public function test_harness_runs(): void
+    {
+        self::assertTrue(true);
+    }
+}
